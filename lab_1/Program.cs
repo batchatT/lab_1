a508bda48b698777dc7ec42b80310f6991801cb4
Dmitrii <dima.moshunov.2002@gmail.com>
2021-10-10 15:41:43 +0300

    first version

---
+++ lab_1/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Linq;
+namespace lb1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool wannaExit = false;
+            Menu choise = 0;
+            Student[] students = new Student[10];
+            int nStudents = 0;
+            while (wannaExit != true)
+            {
+                int i = 0;
+                Console.WriteLine("Оберiть пункт меню: ");
+                foreach (string item in Enum.GetNames(typeof(Menu)))
+                {
+                    Console.WriteLine($"{i++}. {item}");
+                }
+                Console.WriteLine("Що Ви хочете зробити? >>> ");
+                choise = (Menu)int.Parse(Console.ReadLine());
+                switch (choise)
+                {
+                    case Menu.AddStudent:
+                        {
+                            Console.WriteLine("Введiть ПIБ студента: ");
+                            string name = Console.ReadLine();
+                            Console.WriteLine($"Введiть номер залiкової книжки студента {name}: ");
+                            string id = Console.ReadLine();
+                            Console.WriteLine($"Введiть курс студента {name}: ");
+                            int course = int.Parse(Console.ReadLine());
+                            Student stud = new Student(name, id, course);
+                            students[nStudents++] = stud;
+                            break;
+                        }
+                    case Menu.SetMarks:
+                        {
+                            i = 0;
+                            Console.WriteLine("Оберiть який студент має отримати оцiнку: ");
+                            foreach (var student in students)
+                            {
+                                if (students[i] != null) Console.WriteLine($"{i++}. {student.Name}");
+                            }
+                            var choosenStudent = students[int.Parse(Console.ReadLine())];
+                            Console.WriteLine("Оберiть з якого предмету додати оцiнку: ");
+                            i = 0;
+                            foreach (string subject in Enum.GetNames(typeof(Subjects)))
+                            {
+                                Console.WriteLine($"{i++}. {subject}");
+                            }
+                            var choosenSubject = (Subjects)int.Parse(Console.ReadLine());
+                            Console.WriteLine("Введiть оцiнку: ");
+                            choosenStudent.SetMark(choosenSubject, int.Parse(Console.ReadLine()));
+                            break;
+                        }
+                    case Menu.GetMarks:
+                        {
+                            i = 0;
+                            Console.WriteLine("Оберiть студента для виведення оцiнки: ");
+                            foreach (var student in students)
+                            {
+                                if (students[i] != null) Console.WriteLine($"{i++}. {student.Name}");
+                            }
+                            var choosenStudent = students[int.Parse(Console.ReadLine())];
+                            Console.WriteLine("Оберiть з якого предмету вивести оцiнку: ");
+                            i = 0;
+                            foreach (string subject in Enum.GetNames(typeof(Subjects)))
+                            {
+                                Console.WriteLine($"{i++}. {subject}");
+                            }
+                            var choosenSubject = (Subjects)int.Parse(Console.ReadLine());
+                            Console.WriteLine($"Оцiнка: {choosenStudent.GetMark(choosenSubject)}");
+                            break;
+                        }
+                    case Menu.CompareStudents:
+                        {
+                            i = 0;
+                            Console.WriteLine("Оберiть двох студентiв для порiвняння за iндексами: ");
+                            foreach (var student in students)
+                            {
+                                if (students[i] != null) Console.WriteLine($"{i++}. {student.Name}");
+                            }
+                            var chosenStudent1 = students[int.Parse(Console.ReadLine())];
+                            Console.WriteLine($"Перший студент для порiвняння: {chosenStudent1.ToString()}");
+                            var chosenStudent2 = students[int.Parse(Console.ReadLine())];
+                            Console.WriteLine($"Другий студент для порiвняння: {chosenStudent2.ToString()}");
+                            Console.WriteLine("Результат порiвняння оцiнок студентiв: ");
+                            i = 0;
+                            foreach (string subject in Enum.GetNames(typeof(Subjects)))
+                            {
+                                Console.WriteLine($"{i}. {subject}");
+                                Console.WriteLine($"Перший: {chosenStudent1.GetMark((Subjects)Enum.Parse(typeof(Subjects), subject))}");
+                                Console.WriteLine($"Другий: {chosenStudent2.GetMark((Subjects)Enum.Parse(typeof(Subjects), subject))}");
+                                Console.WriteLine($"Рiзниця: {chosenStudent1.CompareTo(chosenStudent2)[i++]}");
+                            }
+                            break;
+                        }
+                    case Menu.ShowInfo:
+                        {
+                            i = 0;
+                            Console.WriteLine("Оберiть студента за iндексом: ");
+                            foreach (var student in students)
+                            {
+                                if (students[i] != null) Console.WriteLine($"{i++}. {student.Name}");
+                            }
+                            Console.WriteLine(students[int.Parse(Console.ReadLine())].ToString());
+                            break;
+                        }
+
+                }
+
+
+
+            }
+        }
+    }
+    enum Menu
+    {
+        AddStudent,
+        SetMarks,
+        GetMarks,
+        CompareStudents,
+        ShowInfo
+    }
+}
---
+++ lab_1/Student.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lb1
+{
+    class Student : IComparable
+    {
+        private string _name;
+        private string _id;
+        private int _course;
+
+        private int[] _marksArr = new int[10];
+        private double _avgMark => _marksArr.Average();
+        public string Name
+        {
+            get => _name;
+
+            set
+            {
+                try
+                {
+                    if (String.IsNullOrWhiteSpace(value))
+                    {
+                        throw new Exception("Iм`я не може бути порожнім");
+                    }
+                    for (int i = 0; i < value.Length; ++i)
+                    {
+                        if (Char.IsDigit(value[i]))
+                        {
+                            throw new Exception("Iм`я не може мiстити цифри");
+                        }
+                    }
+                    _name = value;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Помилка: {e.Message}");
+                }
+            }
+        }
+
+        public string Id
+        {
+            get => _id;
+            set
+            {
+                try
+                {
+                    for (int i = 0; i < value.Length; ++i)
+                    {
+                        if (!Char.IsDigit(value[i]))
+                        {
+                            throw new Exception("Номер залiкової книжки може мiстити тiльки цифри");
+                        }
+                    }
+                    _id = value;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Помилка: {e.Message}");
+                }
+            }
+        }
+
+        public int Course
+        {
+            get => _course;
+            set
+            {
+                try
+                {
+                    if (value <= 0 || value > 6)
+                    {
+                        throw new Exception("Курс повинен знаходитись у діапазоні від 1 до 6 включно");
+                    }
+                    else _course = value;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Помилка: {e.Message}");
+                }
+            }
+        }
+
+        public int[] MarksArr { get => _marksArr; }
+        public Student(string name, string id, int course)
+        {
+            Name = name;
+            Id = id;
+            Course = course;
+        }
+
+        public void SetMark(Subjects subj, int mark) => _marksArr[(int)subj] = mark;
+        public int GetMark(Subjects subj) => _marksArr[(int)subj];
+        public override string ToString() => $"Студент {Name}, залiкова книжка {Id}, курс {Course}";
+        public int[] CompareTo(Student obj)
+        {
+            int[] diffMarks = new int[10];
+            foreach (int i in diffMarks)
+            {
+                diffMarks[i] = this._marksArr[i] - obj._marksArr[i];
+            }
+            return diffMarks;
+        }
+        public int CompareTo(object obj) => throw new NotImplementedException();
+    }
+    enum Subjects
+    {
+        English,
+        Embedded,
+        Web,
+        CSharp,
+        CPlusPlus,
+        ElCircuits,
+        BigData,
+        VHDL,
+        Sports,
+        Philosophy
+    }
+}
+    
